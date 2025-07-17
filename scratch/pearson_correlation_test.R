6f52f5fcd1870049eacdcc3efa2a90665d955339
chloerine <jazores@ucsc.edu>
2025-07-16 20:07:34 -0700

    changed how bioclim variables are read in, cropped it so it is just california

--- scratch/pearson_correlation_test.R
+++ scratch/pearson_correlation_test.R
@@ -1,5 +1,6 @@
 ###README
 #this file contains some scratch code use to test terra and doing pearson correlation coefficieints 
+#also messing around with what you can do in R 
 
 # installing packages needed to work
 # installing terra which is needed to read the raster data from bioclimn
@@ -13,24 +14,37 @@ needpackage <- function(package){
   library(package, character.only = TRUE)
 }
 
+#this funcion takes 
+
 #downloading libraries if needed and setting up libraries
 needpackage("terra")
-needpackage("rnaturalearth")
-needpackage("rnaturalearthdata")
+needpackage("maps")
 needpackage("sf")
+needpackage("rnaturalearth")
 
 #getting the state of california 
 
 # this is reading the bioX variable, 2.1 worldclim values is just raw data (1.4 is scaled by 10)
-bio1 <- rast("data\\wc2.1_30s_bio\\wc2.1_30s_bio_1.tif") #annual mean temp
-bio2 <- rast("data\\wc2.1_30s_bio\\wc2.1_30s_bio_2.tif") #mean diurnal range
+#maybe this can be better found using geodata
 
+# Read all rasters in at once
+input_dir <- "data/wc2.1_30s_bio/"
+files <- list.files(input_dir, pattern = "\\.tif$", full.names = TRUE)
+bioclim_stack <- rast(files)  # Creates a 19-layer SpatRaster
 
+# Get California boundary
+usa <- ne_states(country = "United States of America", returnclass = "sf")
+california <- usa[usa$name == "California", ]
 
-#these inspect the raster
-print(bio1)
-plot(bio1)
-plot(bio2)
+# Convert to SpatVector
+california_vect <- vect(california)
+
+#cropping the bioclim stack
+biocrop <- crop(x = bioclim_stack, y = california_vect)
+
+#masking the biocrop stack
+bioclim_ca <- mask(x = biocrop, mask = california_vect)
+
 
 
 
