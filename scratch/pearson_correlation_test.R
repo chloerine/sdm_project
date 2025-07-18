9ddb39b063b6e23160d5b50f302c98152c2e0bc8
chloerine <jazores@ucsc.edu>
2025-07-18 09:02:22 -0700

    finding all the highly correlated pairs

--- scratch/pearson_correlation_test.R
+++ scratch/pearson_correlation_test.R
@@ -1,31 +1,27 @@
 ###README
 #this file contains some scratch code use to test terra and doing pearson correlation coefficieints 
+# this is for eliminating some paris of bioclim variables 
 #also messing around with what you can do in R 
 
 # installing packages needed to work
 # installing terra which is needed to read the raster data from bioclimn
 
-#this function just downloads and loads the package libraries, takes a string of package name as an input
-needpackage <- function(package){
-  if(!requireNamespace(package)){
-    install.packages(package)
-    message("Installing package: ", package)
+#libraries
+library("terra")
+library("maps")
+library("sf")
+library("rnaturalearth")
+
+#function identifying if the column is a specific number
+id_cor <- function(matrix,num){
+  temp_cor <- list()
+  for (row in 1:nrow(matrix)){
+    if ((matrix[[row,2]] == num)&(matrix[[row,1]]!=num)){
+      temp_cor <- c(temp_cor, matrix[[row,1]])
+    }
   }
-  library(package, character.only = TRUE)
+  return(temp_cor)
 }
-
-#this funcion takes 
-
-#downloading libraries if needed and setting up libraries
-needpackage("terra")
-needpackage("maps")
-needpackage("sf")
-needpackage("rnaturalearth")
-
-#getting the state of california 
-
-# this is reading the bioX variable, 2.1 worldclim values is just raw data (1.4 is scaled by 10)
-#maybe this can be better found using geodata
 
 # Read all rasters in at once
 input_dir <- "data/wc2.1_30s_bio/"
@@ -45,8 +41,31 @@ biocrop <- crop(x = bioclim_stack, y = california_vect)
 #masking the biocrop stack
 bioclim_ca <- mask(x = biocrop, mask = california_vect)
 
+#reordering the bioclim variables so that it in order 1,2,...19
+in_order <- order(as.numeric(gsub("\\D", "", names(bioclim_ca))))
+bioclim_ca <- bioclim_ca[[in_order]]
+names(bioclim_ca) <- paste0("bio", 1:19) #renaming bioclim_ca to bioX where x=1,2,...,19
 
+#extracting the values of the bioclim variables
+bio_val <- values(bioclim_ca)
+bio_matrix <- na.omit(as.matrix(bio_val)) #making into a matrix then deleting all rows where there was a NaN
 
+#checking no na or nan in matrix
+if (any(is.na(bio_matrix))) {
+  message("There is at least one NA or NaN in bio_matrix")
+} else {
+  message("There are no NA or NaN values in bio_matrix")
+}
 
+#computing the pearson correalation, puts it into a matrix
+p_cor_raw <- cor(x = bio_matrix, method = "pearson")
 
+#finding which biolcim variables are highly correlated eliminate higher than 0.75 
+p_cor_abs <- abs(x = p_cor_raw)
+
+#all_list- a list containt all the sublists 1-19 of high correlation variables
+layer_names <- names(bioclim_ca)
+cor_pairs <- expand.grid(var1 = layer_names,var2 = layer_names)
+cor_paris$correlation <- as.vector(p_cor_abs)
+  
 
